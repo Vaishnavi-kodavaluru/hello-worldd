8e574395673f7d7ee5c100f4290ab982cc413f57
Vaishnavi-kodavaluru <53103698+Vaishnavi-kodavaluru@users.noreply.github.com>
2019-07-20 16:42:31 +0530

    Add files via upload

---
+++ 5a.readname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+/**********************************
+*purpose: read name from user and print it
+*********************************/
+void main(){
+char name[30];
+printf("enter the name:");
+scanf("%s",&name);
+printf("your name is : %s",name);
+getch();
+
+}
