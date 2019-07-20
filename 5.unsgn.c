6b6361175689e53bc0e7225922e07f091dd199c3
Vaishnavi-kodavaluru <53103698+Vaishnavi-kodavaluru@users.noreply.github.com>
2019-07-20 16:29:54 +0530

    Add files via upload

---
+++ 5.unsgn.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+unsigned int distance;
+printf("enter the distance between two planets");
+scanf("%u",&distance);
+printf("the distance u entered= %u",distance);
+getch();
+}
